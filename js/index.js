bae1b1b91b095f2ae1ef7e4f94b9d7ead5ced949
Luis1999s <bejamarly@gmail.com>
2021-11-24 22:40:42 -0600

    askisd

--- js/index.js
+++ js/index.js
@@ -1,8 +1,52 @@
 
-  var slideUp = {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '-100px'
-};
 
-ScrollReveal().reveal('.navbar', slideUp);
+  var wn = {
+    duration: 24000,
+    origin: 'left',
+    distance: '400px'
+  };
+  ScrollReveal().reveal('.window-notice', wn);
+  
+  var sa = {
+    duration: 3000,
+  
+    distance: '100px'
+  };
+  ScrollReveal().reveal('.about2', sa);
+  
+  var ssa = {
+    duration: 1000,
+    origin: 'left',
+    distance: '400px'
+  };
+  ScrollReveal().reveal('.services2', ssa);
+  
+  
+  var sf = {
+    duration: 3000,
+    origin: 'top',
+    distance: '400px'
+  };
+  ScrollReveal().reveal('.stat-facts2', sf);
+  
+  var tips = {
+    duration: 3000,
+    origin: 'botton',
+    distance: '400px'
+  };
+  ScrollReveal().reveal('.tips', tips);
+  
+  
+  var t = {
+    duration: 3000,
+    origin: 'left',
+    distance: '500px'
+  };
+  ScrollReveal().reveal('.testimonials2', t);
+  
+  var noticias = {
+    duration: 3000,
+    origin: 'top',
+    distance: '400px'
+  };
+  ScrollReveal().reveal('.noticias2', noticias);
